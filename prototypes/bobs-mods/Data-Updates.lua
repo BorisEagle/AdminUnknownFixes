cc0582801acc22d5f84025a30416bcbe65a2ffcc
BorisEagle <46448975+BorisEagle@users.noreply.github.com>
2026-06-09 22:22:56 +0200

    Use temporary Bob underground belt shim during data-updates

--- prototypes/bobs-mods/Data-Updates.lua
+++ prototypes/bobs-mods/Data-Updates.lua
@@ -1,5 +1,14 @@
 
+require('functions/fix-missing-bob-underground-belts')
+if type(_G.__auf_install_bob_underground_belt_shim) == 'function' then
+	_G.__auf_install_bob_underground_belt_shim()
+end
+
 require('prototypes/bobs-mods/prototypes/overrides/overrides-updates')
+
+if type(_G.__auf_restore_bob_underground_belt_shim) == 'function' then
+	_G.__auf_restore_bob_underground_belt_shim()
+end
 
 if mods['angelsrefining'] then
 	require('prototypes/bobs-mods/prototypes/overrides/angels/overrides-updates')
